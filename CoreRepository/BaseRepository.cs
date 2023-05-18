50868711d90f2d9e489d14fa3dc68c7e7df12ce5
TahaYasinOzturk <tahayasinozturk@hotmail.com>
2023-05-18 23:32:00 +0300

    Add CoreRepository

---
+++ CoreRepository/BaseRepository.cs
@@ -0,0 +1,91 @@
+﻿using AdvanceRepository.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvanceRepository.CoreRepository
+{
+    // IBaseRepository e ctrl . dedik impelement ifterface sectik otomatik olusturdu.
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly Context context;
+
+        //burda contex i cekebilmek icin program cs de AddDBContext icine yazmak
+
+        // alltaki metodları tanımladık  sonra repository klasörü olusturduk orda abstract lara soyut digerleri icin somut  lara concrete
+        public BaseRepository(Context context)
+        {   //Context = context e eşitledik veritabanının bagladık.
+            this.context = context;
+        }
+        public bool Add(T entity)
+        {
+            //save changes i buraya deil c# da yapıcaz transaction yapıcaz.
+            try
+            {
+                // ilk yaptıgımız reposuz hali
+                //Categories cat;
+
+
+                //context.Set<T>().Add(entity); asagıdakinin uzun hali.
+
+
+                Set().Add(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+
+
+        public bool Delete(T entity)
+        {
+            try
+            {
+                Set().Remove(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public T Find(int id)
+        {
+            return Set().Find(id);
+        }
+
+        public T Find(Guid Id)
+        {
+            return Set().Find(Id);
+        }
+
+        public List<T> List()
+        {
+            return Set().ToList();
+        }
+
+        public DbSet<T> Set()
+        {
+            //return _context.Set<T>();
+            return context.Set<T>();
+        }
+
+        public bool Update(T entity)
+        {
+            try
+            {
+                Set().Update(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+    }
+}
---
+++ CoreRepository/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdvanceRepository.CoreRepository
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        //interface de public yazmıyoruz.
+        public List<T> List();
+        public T Find(int id);
+        // int de olabilir guid de olabilir. overload icin 2 tane yazıcaz.
+        public T Find(Guid Id);
+        public bool Add(T entity);
+        public bool Update(T entity);
+        public bool Delete(T entity);
+        public DbSet<T> Set();
+    }
+}
