71c5d27027794499f4b2cbd4706aa14644f5635e
TahaYasinOzturk <tahayasinozturk@hotmail.com>
2023-05-21 23:10:56 +0300

    Add ProductsModel

---
+++ Models/ViewModel/ProductsModel.cs
@@ -0,0 +1,20 @@
+﻿using AdvanceRepository.Models.Classes;
+
+namespace AdvanceRepository.Models.ViewModel
+{
+    public class ProductsModel
+    {
+        public ProductsModel()
+        {
+            SelectedProducts = new Products();
+        }
+        public Products SelectedProducts { get; set; }
+        public List<Categories> Categories { get; set; }
+
+
+        public string? Head { get; set; }
+        public string? Text { get; set; }
+        public string? Class { get; set; }
+
+    }
+}
